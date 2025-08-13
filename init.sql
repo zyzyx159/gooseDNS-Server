7289e44f939a3714cdeec958b85ad022537394ac
Daniel Stellmon <Dstellm@gmail.com>
2025-08-13 09:32:21 -0500

    added demo data

--- init.sql
+++ init.sql
@@ -9,11 +9,41 @@ CREATE TABLE domains(
     wan_IP VARCHAR
 );
 
+Insert into domains(
+    domain,
+    email, 
+    sn, 
+    refresh,
+    retry,
+    expire,
+    min_ttl,
+    wan_IP,
+    ) VALUES (
+        'example.com.',
+        'admin.example.com.',
+        1,
+        10800,
+        1800,
+        1209600,
+        10800,
+        '23.215.0.138'
+    );
+
 CREATE TABLE subdomains(
     subdomain VARCHAR,
     active BOOLEAN,
     domain VARCHAR,
     PRIMARY KEY (domain, subdomain)
+);
+
+INSERT into subdomains(
+    subdomain,
+    active,
+    domain
+) VALUE (
+    'sub',
+    TRUE,
+    'example.com.'
 );
 
 CREATE TABLE IPaddr(
@@ -31,4 +61,16 @@ CREATE TABLE records(
     subdomain VARCHAR,
     recordType VARCHAR,
     recordValue VARCHAR
+);
+
+INSERT into records(
+    domain,
+    subdomain,
+    recordType,
+    recordValue
+) VALUES (
+    'example.com.',
+    'sub',
+    'A',
+    'IP'
 );
